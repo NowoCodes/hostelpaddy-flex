9477f2644234d1621b18d2586135496b2db830d5
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-03-29 20:09:17 +0100

    Add user and admin views and functionality for email collection

---
+++ app/Http/Controllers/UserEmailCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Email;
+
+class UserEmailCollection extends Controller
+{
+    public function create()
+    {
+        return view('clientmail');
+    }
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required|string|email|max:255'
+        ]);
+        Email::create($request->all());
+
+        return view('thanks');
+    }
+}
---
+++ resources/views/clientmail.blade.php
@@ -0,0 +1,40 @@
+@extends('emails.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="float-left">
+                <h2>Add New Email</h2>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="/clientmail" method="POST">
+        @csrf
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Email:</strong>
+                    <input type="email" name="email" class="form-control" placeholder="Email">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-sm btn-primary">Submit</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
