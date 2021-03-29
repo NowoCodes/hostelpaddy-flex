cfcb5b624e4a9abdfd79add810c2f55c7f7012da
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-03-29 15:03:01 +0100

    Create EmailCollectionController.php

---
+++ app/Http/Controllers/EmailCollectionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmailCollectionController extends Controller
+{
+    public function create() {
+        return view('email');
+    }
+
+    public function store() {
+
+    }
+}
+
