0d7a316d9067371caaa29b63355b90bbc5fa2374
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-03-29 19:30:49 +0100

    Create thanks.blade.php

---
+++ resources/views/emails/thanks.blade.php
@@ -0,0 +1,9 @@
+@extends('emails.layout')
+
+@section('content')
+    <div class="text-center">
+        <h2>THANK YOU</h2>
+    </div>
+
+    <p class="text-center">Your email has been collected succesfully</p>
+@endsection
