e3c1d843a0d93cf7465e149e23ed3d0794ade208
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-03-29 17:00:42 +0100

    create email collection views

---
+++ resources/views/emails/index.blade.php
@@ -0,0 +1,50 @@
+@extends('emails.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Email Collection</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('products.create') }}">Add Email</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Email</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $emails->email }}</td>
+            <td>
+                <form action="{{ route('emails.destroy',$product->id) }}" method="POST">
+
+                    <a class="btn btn-info" href="{{ route('emails.show',$product->id) }}">Show</a>
+
+                    <a class="btn btn-primary" href="{{ route('emails.edit',$product->id) }}">Edit</a>
+
+                    @csrf
+                    @method('DELETE')
+
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+
+    {!! $products->links() !!}
+
+@endsection
