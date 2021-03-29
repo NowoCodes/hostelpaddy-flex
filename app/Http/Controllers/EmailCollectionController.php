5132b2dbc833f6e58f4c28586c78c67299691792
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-03-29 17:45:49 +0100

    Update EmailCollectionController.php

--- app/Http/Controllers/EmailCollectionController.php
+++ app/Http/Controllers/EmailCollectionController.php
@@ -2,16 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Email;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class EmailCollectionController extends Controller
 {
-    public function create() {
-        return view('email');
+    public function index () {
+        $emails = Email::all();
+        return view('emails.index', compact('emails'));
     }
 
-    public function store() {
+    public function create ()
+    {
+        return view('emails.create');
+    }
 
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required|string|email|max:255'
+        ]);
+        Email::create($request->all());
+
+        return redirect()->route('emails.index')
+            ->with('success', 'Project created successfully.');
+        // back()->with('post_created', 'Email has been submitted successfully');
+        // return view('email_thanks');
     }
 }
-
