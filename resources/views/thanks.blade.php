9c094019a35533b71c86074e670a66fb4998d1d6
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-10 12:21:34 +0100

    update with production site

--- resources/views/thanks.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('emails.layout')
-
-@section('content')
-    <div class="text-center">
-        <h2>THANK YOU</h2>
-    </div>
-
-    <p class="text-center">Your email {{ $email }} has been collected succesfully</p>
-@endsection
