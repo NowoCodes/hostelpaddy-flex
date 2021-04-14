6992a011f8c492a325e842bb3484c871190f20a9
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-04-14 02:07:46 +0100

    dynamically fetch contents in select menus

--- resources/views/agents/hostels/form.blade.php
+++ resources/views/agents/hostels/form.blade.php
@@ -1,101 +1,77 @@
 <div class="form-group">
-  <label for="hname">Hostel Name</label>
-  <input type="text" class="form-control" id="hname" placeholder="Type your hostel name here">
+  <label for="h_name">Hostel Name</label>
+  <input type="text" class="form-control" id="h_name" name="hostel_name" placeholder="Type your hostel name here"
+    value="{{ old('hostel_name', $hostel->hostel_name) }}">
 </div>
 <div class="form-group">
   <label for="hostelname">University Name</label>
-  <select name="hostelname" class="form-control">
-    <option>Select your university name</option>
-    <option>Legacy University, Okija</option>
-    <option>Madonna University, Okija</option>
-    <option>Nnamdi Azikiwe University</option>
-    <option>Tansian University</option>
-    <option>Eastern Palm University</option>
-    <option>Federal University of Technology, Owerri</option>
-    <option>Federal University of Technology, Minna</option>
-    <option>Imo State University</option>
-    <option>Ignatius Ajuru University of Education</option>
-    <option>Rivers State University </option>
-    <option>University of Port Harcourt </option>
+  <select id="uni_name" name="uni_name" class="form-control">
+    <option value="">Select your university name</option>
+    @foreach ($university as $universities)
+      <option value="{{ $universities->id }}"
+        {{ $universities->id == old('uni_name', $hostel->uni_name) ? 'selected' : '' }}>
+        {{ $universities->university }}</option>
+    @endforeach
   </select>
 </div>
 
 <div class="row">
   <div class="col-6">
-
     <div class="form-group">
       <label for="state">State</label>
-      <select class="form-control" id="sel1" name="state">
-        <option selected>Select one</option>
-        <option value="Ogun">Ogun</option>
-        <option value="Calabar">Calabar</option>
-        <option value="Osun">Osun</option>
-        <option value="Lagos">Lagos</option>
+      <select class="form-control" id="state" name="state">
+        <option value="">Select one</option>
+        @foreach ($state as $states)
+          <option value="{{ $states->id }}" {{ $states->id == old('state', $hostel->state) ? 'selected' : '' }}>
+            {{ $states->state }}</option>
+        @endforeach
       </select>
     </div>
 
-    {{-- <div class="form-group">
-          <label for="hname">State</label>
-          <input type="text" class="form-control" id="hname">
-        </div> --}}
-
-
     <div class="form-group">
-      <label for="hosteladdress">Hostel Address</label>
-      <input type="text" class="form-control" id="pwd" name="address" placeholder="What’s your hostel address">
+      <label for="address">Hostel Address</label>
+      <input type="text" class="form-control" id="address" name="address" placeholder="What’s your hostel address"
+        value="{{ old('address', $hostel->address) }}">
     </div>
   </div>
   <div class="col-6">
     <div class="form-group">
       <label for="city">City or Town</label>
-      <input type="text" class="form-control" id="city" name="city" placeholder="Enter city or town">
+      <input type="text" class="form-control" id="city" name="city" placeholder="Enter city or town"
+        value="{{ old('city', $hostel->city) }}">
     </div>
     <div class="form-group">
       <label for="price">Hostel Price</label>
-      <input type="text" class="form-control" id="price" placeholder="₦ 250,000 ">
+      <input type="number" class="form-control" id="price" name="price" placeholder="₦250,000"
+        value="{{ old('price', $hostel->price) }}">
     </div>
   </div>
 </div>
 
 <div class="form-group">
-  <label for="details">Hostel Details</label>
-  <textarea class="form-control" name="details" id="details" placeholder="Tell us more about your hostel"></textarea>
-</div>
-
-<div class="row">
-  <div class="col-6">
-    <div class="form-group">
-      <label for="email">Email Address</label>
-      <input type="text" class="form-control" id="email" name="email" placeholder="favour@gmail.com">
-    </div>
-  </div>
-  <div class="col-6">
-    <div class="form-group">
-      <label for="phone">Phone Number</label>
-      <input type="text" class="form-control" id="phone" name="phone" placeholder="Enter your phone number">
-    </div>
-  </div>
+  <label for="description">Hostel Description</label>
+  <textarea class="form-control" name="description" id="description"
+    placeholder="Tell us more about your hostel">{{ old('description', $hostel->description) }}</textarea>
 </div>
 
 <h5>Hostel Amenities</h5>
-<label>Select your hostel amenities below</label>
+<label for="amenities">Select your hostel amenities below</label>
 <div class="form-group">
-  <select class="form-control mul-select" multiple="true" name="amenities[]">
-    <option>Select Amenities</option>
-    <option value="transformer">Transformer</option>
-    <option value="waterwell">Water Well</option>
-    <option value="waterpump">Water Pump</option>
-    <option value="generator">Generator</option>
-    <option value="kitchen">Kitchen</option>
-    <option value="toilet">Toilet</option>
-    <option value="wardrobe">Wardrobe</option>
-    <option value="balcony">Balcony</option>
-    <option value="bathroom">Bathroom</option>
+  {{-- <select class="form-control mul-select" multiple="true" id="amenities" name="amenities[]"> --}}
+  {{-- Having problems with passing arrays into a table colum hence commenting the above line --}}
+  <select class="form-control" id="amenities" name="amenities">
+    <option value="">Select Amenities</option>
+    @foreach ($amenity as $amenities)
+      <option value="{{ $amenities->id }}"
+        {{ $amenities->id == old('amenities', $hostel->amenities) ? 'selected' : '' }}>{{ $amenities->amenity }}
+      </option>
+    @endforeach
   </select>
 
   <h5 class="mt-2">Upload Hostel Images</h5>
   <div class="custom-file mb-3">
-    <label class="custom-file-label" for="customFile">Choose file</label>
-    <input type="file" class="custom-file-input" id="customFile" name="filename">
+    <label class="custom-file-label" for="image_name">Choose file</label>
+    <input type="file" class="custom-file-input" id="image_name" name="image_name"
+      value="{{ old('image_name', $hostel->image_name) }}">
   </div>
 </div>
